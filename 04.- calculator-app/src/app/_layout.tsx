36f15bc0883a75dc7d4f471492107191187b931d
ING-Mike-Programador <miguel_alonso44@hotmail.com>
2026-09-23 16:50:15 -0700

    Actualizacion: reestructurar la aplicación eliminando componentes innecesarios y simplificando el layout

--- 04.- calculator-app/src/app/_layout.tsx
+++ 04.- calculator-app/src/app/_layout.tsx
@@ -1,18 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
+import { Slot } from "expo-router";
+import { Text, View } from "react-native";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-SplashScreen.preventAutoHideAsync();
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+const RootLayout = () => {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <View>
+      <Text>HEADER</Text>
+
+      <Slot />
+
+      <Text> FOOTER</Text>
+    </View>
   );
-}
+};
+
+export default RootLayout;
