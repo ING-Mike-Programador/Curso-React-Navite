13fba585cbd091c75604ebf1d298f782d3125dc1
ING-Mike-Programador <miguel_alonso44@hotmail.com>
2026-09-23 22:22:38 -0700

    Actualizacion: reorganizar la lógica para ocultar la barra de navegación en RootLayout según el sistema operativo

--- 04.- calculator-app/src/app/_layout.tsx
+++ 04.- calculator-app/src/app/_layout.tsx
@@ -1,11 +1,15 @@
 import { NavigationBar } from "expo-navigation-bar";
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Platform, Text, View } from "react-native";
 import { globalStyles } from "../../styles/global-styles";
 
-// Ocultar barra de opciones en el movil
-NavigationBar.setHidden(true);
+// Verificar si el dispositivo es android
+const isAndroid = Platform.OS === "android";
+
+// Ocultar barra de opciones en el movil android
+if (isAndroid) NavigationBar.setHidden(true);
+
 const RootLayout = () => {
   return (
     <View style={globalStyles.container}>
