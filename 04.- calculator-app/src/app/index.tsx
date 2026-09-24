d6432e46b2be12785608e662e7a096842895baed
ING-Mike-Programador <miguel_alonso44@hotmail.com>
2026-09-23 17:03:09 -0700

    Actualizacion: mejorar el estilo del texto en el componente CalculatorAPP

--- 04.- calculator-app/src/app/index.tsx
+++ 04.- calculator-app/src/app/index.tsx
@@ -1,9 +1,12 @@
+import { Fonts } from "@/constants/theme";
 import { Text, View } from "react-native";
 
 const CalculatorAPP = () => {
   return (
     <View>
-      <Text>CalculatorAPP</Text>
+      <Text style={{ fontSize: 50, fontFamily: Fonts.mono }}>
+        CalculatorAPP
+      </Text>
     </View>
   );
 };
