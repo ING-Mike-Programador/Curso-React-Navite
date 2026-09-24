f17be87ca7e1d97f26781b5dc8368f6378681850
ING-Mike-Programador <miguel_alonso44@hotmail.com>
2026-09-23 22:20:45 -0700

    Actualizacion: agregar dependencias expo-haptics y expo-navigation-bar en package.json y package-lock.json; ocultar barra de navegación en RootLayout

--- 04.- calculator-app/src/app/_layout.tsx
+++ 04.- calculator-app/src/app/_layout.tsx
@@ -1,8 +1,11 @@
+import { NavigationBar } from "expo-navigation-bar";
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { globalStyles } from "../../styles/global-styles";
 
+// Ocultar barra de opciones en el movil
+NavigationBar.setHidden(true);
 const RootLayout = () => {
   return (
     <View style={globalStyles.container}>
