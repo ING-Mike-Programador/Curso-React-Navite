b693347e74e1ba079b7ca28f0f1ca29d672cfb71
ING-Mike-Programador <miguel_alonso44@hotmail.com>
2026-09-23 18:46:01 -0700

    Actualizacion: implementar el componente ThemeText en CalculatorAPP para mejorar la gestión de estilos de texto

---
+++ 04.- calculator-app/components/ThemeText.tsx
@@ -0,0 +1,24 @@
+import { Colors } from "@/constants/theme";
+import { Text, TextProps } from "react-native";
+import { globalStyles } from "../styles/global-styles";
+interface Props extends TextProps {
+  variant?: "h1" | "h2";
+}
+
+const ThemeText = ({ children, variant, ...rest }: Props) => {
+  return (
+    <Text
+      style={[
+        { color: Colors.text.secondary },
+        variant === "h1" && globalStyles.mainResult,
+        variant === "h2" && globalStyles.subResult,
+      ]}
+      numberOfLines={1}
+      adjustsFontSizeToFit
+      {...rest}
+    >
+      {children}
+    </Text>
+  );
+};
+export default ThemeText;
--- 04.- calculator-app/src/app/index.tsx
+++ 04.- calculator-app/src/app/index.tsx
@@ -1,17 +1,12 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import ThemeText from "../../components/ThemeText";
 import { globalStyles } from "../../styles/global-styles";
 
 const CalculatorAPP = () => {
   return (
     <View style={globalStyles.calculatorContainer}>
-      <Text
-        style={globalStyles.mainResult}
-        numberOfLines={1}
-        adjustsFontSizeToFit
-      >
-        50 X 500000000
-      </Text>
-      <Text style={globalStyles.subResult}>2500</Text>
+      <ThemeText variant="h1">50 X 500000000</ThemeText>
+      <ThemeText variant="h2">2500</ThemeText>
     </View>
   );
 };
